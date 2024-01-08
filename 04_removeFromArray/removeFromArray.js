ebe5291df572b0f09fb042a5fb729b993b288768
Wipram Gamurci <wipramgamurci@gmail.com>
2024-01-08 16:42:31 +0800

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...elements) {
+  const resultArray = arr.filter((nilai) => !elements.includes(nilai));
+  return resultArray;
 };
 
 // Do not edit below this line
