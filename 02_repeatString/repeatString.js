5401b0b7c91b5cd1de53152d1dab3fae6f81886b
Wipram Gamurci <wipramgamurci@gmail.com>
2024-01-08 14:22:31 +0800

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let repeated = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      repeated += str;
+    }
+    return repeated;
+  }
 };
 
 // Do not edit below this line
