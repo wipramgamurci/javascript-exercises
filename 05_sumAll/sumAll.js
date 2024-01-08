5aa46d9fd6fb5bb1fac1bb62220424542dac69fc
Wipram Gamurci <wipramgamurci@gmail.com>
2024-01-08 17:15:12 +0800

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
 
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
+
+  if (a > b) {
+    let x = a;
+    a = b;
+    b = x;
+  }
+
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
