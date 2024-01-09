20b675b59ec681784bd43e130e491e3c802de884
Wipram Gamurci <wipramgamurci@gmail.com>
2024-01-09 14:36:07 +0800

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const converted = (temp - 32) * (5 / 9);
+  const rounded = Math.round(converted * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const converted = temp * (9 / 5) + 32;
+  const rounded = Math.round(converted * 10) / 10;
+  return rounded;
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
