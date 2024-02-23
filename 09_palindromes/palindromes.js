77022e1dba78bc2a9f8d5b55f314988408a7f860
Wipram Gamurci <wipramgamurci@gmail.com>
2024-02-23 17:23:47 +0800

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const proccessedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return proccessedString.split("").reverse().join("") === proccessedString;  
 };
 
 // Do not edit below this line
