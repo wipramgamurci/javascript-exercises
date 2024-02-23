a8988b2091626e013edb74f80fa58bc3041142d2
Wipram Gamurci <wipramgamurci@gmail.com>
2024-02-23 17:03:31 +0800

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current, 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1
+  }
+  let result = 1;
+  for (let i = 1; i <= num; i++){
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
