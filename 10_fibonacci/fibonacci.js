de30f7b8582fd7b99bf9eb62fb0b53cb716b0156
Wipram Gamurci <wipramgamurci@gmail.com>
2024-02-24 23:18:39 +0800

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  const num = parseInt(input);
 
+  if(num === 0) return 0;
+  if(num < 0) return "OOPS"
+
+  let secondPrevious = 0;
+  let firstPrevious = 1;
+
+  for (let i = 2; i <= num; i++){
+    let current = firstPrevious + secondPrevious;
+    secondPrevious = firstPrevious;
+    firstPrevious = current;
+  }
+  return firstPrevious;
 };
 
 // Do not edit below this line
